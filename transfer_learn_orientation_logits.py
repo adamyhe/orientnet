4d8a1fd686cfc574f16e82c1a6a6a45f46abb1d1
Adam Youlin He <ayh8@cornell.edu>
2024-11-13 15:14:20 -0500

    Update transfer_learn_orientation_logits.py

--- transfer_learn_orientation_logits.py
+++ transfer_learn_orientation_logits.py
@@ -30,8 +30,6 @@ def warmup_lr(epoch, lr):
     print(f"LEARNING RATE = {lr}")
     if epoch < 1:
         return lr / 10
-    elif epoch == 1:
-        return lr * 10
     else:
         return lr
 
